ad16016c707784fc3a45cdbe511862fff1a9d67f
Ron Panduwana <panduwana@gmail.com>
2015-01-30 23:43:48 +0700

    Initial version: only supports atomic ? quantifier

---
+++ oprex.py
@@ -0,0 +1,215 @@
+import regex, argparse
+from ply import lex, yacc
+
+
+def oprex(source_code):
+    source_code = check_input(source_code)
+    lexer = build_lexer(source_code)
+    result = parse(lexer=lexer)
+    return result
+
+
+class OprexSyntaxError(Exception):
+    def __init__(self, lineno, msg):
+        if lineno:
+            Exception.__init__(self, 'Line %d: %s' % (lineno, msg))
+        else:
+            Exception.__init__(self, msg)
+
+
+def check_input(source_code):
+    source_lines = source_code.split('\n')
+    numlines = len(source_lines)
+
+    # we require the oprex source code to have leading and trailing empty lines
+    # to make proper look of indentation when it is a triple-quotes string
+    if source_lines[0].strip():
+        raise OprexSyntaxError(1, 'First line must be blank, not: ' + source_lines[0])
+    if source_lines[-1].strip():
+        raise OprexSyntaxError(numlines, 'Last line must be blank, not: ' + source_lines[-1])    
+    return source_code
+
+
+# The followings are PLY-related
+
+tokens = (
+    'EQUALS',
+    'INDENT',
+    'LITERAL',
+    'NEWLINE',
+    'QUEST',
+    'SLASH',
+    'VARIABLE',
+)
+
+t_ignore = '' # oprex is whitespace-significant, no ignored characters
+
+
+def t_LITERAL(t):
+    r"""(?:'|")(.*)"""
+    value = t.value.strip()
+    if len(value) < 2 or value[0] != value[-1]:
+        raise OprexSyntaxError(t.lineno, 'Missing closing quote: ' + value)
+    t.value = value[1:-1] # remove the surrounding quotes
+    return t
+
+
+def t_NEWLINE(t):
+    r'([ \t]*\n)+'
+    t.lexer.lineno += t.lexer.lexmatch.group(0).count('\n')
+    return t
+
+
+class Variable(str):
+    pass
+
+def t_VARIABLE(t):
+    r'[A-Za-z0-9_]+'
+
+    name = t.value
+    if regex.match('[0-9_]', name):
+        raise OprexSyntaxError(t.lineno, 'Illegal variable name (must start with a letter): ' + name)
+    if name[-1] == '_':
+        raise OprexSyntaxError(t.lineno, 'Illegal variable name (must not end with underscore): ' + name)
+    t.value = Variable(name)
+    return t
+
+
+# Rules that may eat space/tab 
+# should be using the function form and be put before the INDENT rule
+# to make it run before INDENT
+
+def t_EQUALS(t):
+    r'[ \t]*=[ \t]*'
+    return t
+
+
+def t_INDENT(t):
+    r'([ ]+|[\t]+)(?=(?P<indentee>.*))'
+    indenter = t.value[0]
+    try:
+        if indenter != t.lexer.indenter:
+            raise OprexSyntaxError(t.lineno, 'Cannot mix space and tab for indentation')
+    except AttributeError:
+        # this is the first INDENT encountered, record what character is used for indentation
+        # further indentation must use the same character
+        t.lexer.indenter = indenter
+
+    indentee = t.lexer.lexmatch.group('indentee')
+    if not indentee:
+        return # indent requires something follows, i.e. indented empty line should not produce INDENT token
+    return t
+
+
+t_QUEST = r'\?'
+t_SLASH = r'/'
+
+
+def t_error(t):
+    raise OprexSyntaxError(t.lineno, 'Unsupported syntax: ' + t.value.split('\n')[0])
+
+
+def p_oprex(t):
+    '''oprex : 
+             | NEWLINE
+             | NEWLINE expression
+             | NEWLINE INDENT expression'''
+    if len(t) <= 2:
+        t[0] = ''
+    else:
+        t[0] = t[len(t)-1]
+
+
+def p_expression(t):
+    '''expression : VARIABLE NEWLINE definitions
+                  | chain    NEWLINE definitions'''
+    try:
+        if isinstance(t[1], Variable):
+            t[0] = t.lexer.vars[t[1]]
+        else:
+            t[0] = t[1] % t.lexer.vars
+
+    except KeyError as e:
+        raise OprexSyntaxError(t.lineno(0), "Variable '%s' is not defined" % e.message)
+
+
+def p_chain(t):
+    '''chain : SLASH cell moreCells'''
+    t[0] = t[2] +t[3]
+
+
+def p_cell(t):
+    '''cell : VARIABLE SLASH
+            | VARIABLE QUEST SLASH'''
+    t[0] = '%(' + t[1] + ')s'
+    optional = t[2] == '?'
+    if optional:
+        t[0] = '(?:%s)?+' % t[0]
+
+
+def p_moreCells(t):
+    '''moreCells : 
+                 | cell moreCells'''
+    try:
+        t[0] = t[1] + t[2]
+    except IndexError: # empty moreCells
+        t[0] = ''
+
+
+def p_definitions(t):
+    '''definitions : 
+                   | INDENT definition NEWLINE definitions'''
+
+
+def p_definition(t):
+    '''definition : VARIABLE EQUALS definition
+                  | VARIABLE EQUALS expression
+                  | VARIABLE EQUALS LITERAL'''
+    defined_vars = t.lexer.vars
+    try:
+        defined_vars[t[1]]
+    except KeyError:
+        pass
+    else:
+        raise OprexSyntaxError(t.lineno(1), "Variable '%s' already defined (names must be unique within a scope)" % t[1])
+
+    defined_vars[t[1]] = t[3]
+    t[0] = t[3]
+
+
+def p_error(t):
+    if t is None:
+        raise OprexSyntaxError(None, 'Unexpected end of input')
+    errline = t.lexer.lexdata.split('\n')[t.lineno - 1]
+    pointer = ' ' * (find_column(t)-1) + '^'
+    raise OprexSyntaxError(t.lineno, 'Unexpected token %s\n%s\n%s' % (t.type, errline, pointer))
+
+
+def find_column(t):
+    last_newline = t.lexer.lexdata.rfind('\n', 0, t.lexpos)
+    if last_newline < 0:
+        last_newline = 0
+    return t.lexpos - last_newline
+
+
+lexer0 = lex.lex()
+def build_lexer(source_code):
+    lexer = lexer0.clone()
+    lexer.vars = {}
+    lexer.input(source_code)
+    return lexer
+
+
+parser = yacc.yacc()
+def parse(lexer):
+    return parser.parse(lexer=lexer, tracking=True)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('path/to/source/file')
+    args = argparser.parse_args()
+    source_file = getattr(args, 'path/to/source/file')
+    with open(source_file, 'r') as f:
+        source_code = f.read() 
+    print oprex(source_code)
---
+++ tests.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+
+import unittest, regex
+from oprex import oprex, OprexSyntaxError
+
+class TestErrorHandling(unittest.TestCase):
+    def given(self, oprex_source, expect_error):
+        try:
+            oprex(oprex_source)
+        except Exception as err:
+            got_error = err.message
+        else:
+            got_error = ''
+
+        if got_error != expect_error:
+            msg = 'For input: %s\n----------------------------- Got Error: -----------------------------\n%s\n\n-------------------------- Expected Error: ---------------------------\n%s'
+            raise AssertionError(msg % (
+                oprex_source or '(empty string)', 
+                got_error or '(no error)', 
+                expect_error or '(no error)',
+            ))
+
+
+    def test_white_guards(self):
+        self.given('one-liner input',
+        expect_error='''Line 1: First line must be blank, not: one-liner input''')
+
+        self.given('''something in the first line
+        ''',
+        expect_error='Line 1: First line must be blank, not: something in the first line')
+
+        self.given('''
+        something in the last line''',
+        expect_error='Line 2: Last line must be blank, not:         something in the last line')
+
+
+    def test_unknown_symbol(self):
+        self.given('''
+            `@#$%^&;{}[]\\
+        ''',
+        expect_error='Line 2: Unsupported syntax: `@#$%^&;{}[]\\')
+
+
+    def test_unexpected_token(self):
+        self.given('''
+            /to/be/?
+        ''',
+        expect_error='''Line 2: Unexpected token QUEST
+            /to/be/?
+                   ^''')
+
+
+    def test_mixed_indentation(self):
+        self.given('''
+            \tthis_line_mixes_tab_and_spaces_for_indentation
+        ''',
+        expect_error='Line 2: Cannot mix space and tab for indentation')
+
+        self.given('''
+            /tabs/vs/spaces/
+\t\ttabs = 'this line is tabs-indented'
+                spaces = 'this line is spaces-indented'
+        ''',
+        expect_error='Line 3: Cannot mix space and tab for indentation')
+
+
+    def test_undefined_variable(self):
+        self.given('''
+            bigfoot
+        ''',
+        expect_error="Line 2: Variable 'bigfoot' is not defined")
+
+        self.given('''
+            /horses/and/unicorns/
+                horses = 'Thoroughbreds'
+                and = ' and '
+        ''',
+        expect_error="Line 2: Variable 'unicorns' is not defined")
+
+        self.given('''
+            /unicorns/and/horses/
+                horses = 'Thoroughbreds'
+                and = ' and '
+        ''',
+        expect_error="Line 2: Variable 'unicorns' is not defined")
+
+
+    def test_illegal_variable_name(self):
+        self.given('''
+            101dalmatians
+        ''',
+        expect_error='Line 2: Illegal variable name (must start with a letter): 101dalmatians')
+
+        self.given('''
+            _this_
+        ''',
+        expect_error='Line 2: Illegal variable name (must start with a letter): _this_')
+
+        self.given('''
+            etc_
+        ''',
+        expect_error='Line 2: Illegal variable name (must not end with underscore): etc_')
+
+
+    def test_duplicate_variable(self):
+        self.given('''
+            dejavu
+                dejavu = 'Déjà vu'
+                dejavu = 'Déjà vu'
+        ''',
+        expect_error="Line 4: Variable 'dejavu' already defined (names must be unique within a scope)")
+
+
+    def test_unclosed_literal(self):
+        self.given('''
+            mcd
+                mcd = 'McDonald's
+        ''',
+        expect_error='''Line 3: Missing closing quote: 'McDonald's''')
+
+        self.given('''
+            quotes_mismatch
+                quotes_mismatch = "'
+        ''',
+        expect_error="""Line 3: Missing closing quote: "'""")
+
+
+class TestOutput(unittest.TestCase):
+    def given(self, oprex_source, expect_regex):
+        regex_source = oprex(oprex_source)
+        if regex_source != expect_regex:
+            msg = 'For input: %s\n---------------------------- Got Output: -----------------------------\n%s\n\n------------------------- Expected Output: ---------------------------\n%s'
+            raise AssertionError(msg % (
+                oprex_source or '(empty string)', 
+                regex_source or '(empty string)', 
+                expect_regex or '(empty string)',
+            ))
+
+
+    def test_empties(self):
+        self.given('',
+        expect_regex='')
+
+        self.given('''
+        ''',
+        expect_regex='')
+
+        self.given('''
+
+        ''',
+        expect_regex='')
+        self.given('''
+
+
+        ''',
+        expect_regex='')
+
+
+class TestMatches(unittest.TestCase):
+    def given(self, oprex_source, expect_full_match, no_match=[], partial_match={}):
+        regex_source = oprex(oprex_source)
+        for text in expect_full_match:
+            match = regex.match(regex_source, text)
+            partial = match and match.group(0) != text
+            if not match or partial:
+                raise AssertionError('%s\nis expected to fully match: %s\n%s\nThe regex is: %s' % (
+                    oprex_source or '(empty string)', 
+                    text or '(empty string)', 
+                    'It does match, but only partially. The match is: ' + (match.group(0) or '(empty string)') if partial else "But it doesn't match at all.",
+                    regex_source or '(empty string)',
+                ))
+
+        for text in no_match:
+            match = regex.match(regex_source, text)
+            if match:
+                raise AssertionError('%s\nis expected NOT to match: %s\n%s\nThe regex is: %s' % (
+                    oprex_source or '(empty string)', 
+                    text or '(empty string)', 
+                    'But it does match. The match is: ' + (match.group(0) or '(empty string)'),
+                    regex_source or '(empty string)',
+                ))
+
+
+        for text, partmatch in partial_match.iteritems():
+            match = regex.match(regex_source, text)
+            partial = match and match.group(0) != text and match.group(0) == partmatch
+            if not match or not partial:
+                raise AssertionError('%s\nis expected to partially match: %s\n%s\nThe regex is: %s' % (
+                    oprex_source or '(empty string)', 
+                    text or '(empty string)', 
+                    "But it doesn't match at all." if not match else 'The expected partial match is: %s\nBut the resulting match is: %s' % (
+                        partmatch or '(empty string)', 
+                        match.group(0) or '(empty string)'
+                    ),
+                    regex_source or '(empty string)',
+                ))
+
+
+    def test_simple_optional(self):
+        self.given('''
+            /a?/ether/
+                a = 'a'
+                ether = 'ether'
+            ''',
+            expect_full_match=['ether', 'aether'],
+            no_match=['tether'],
+        )
+
+        self.given('''
+            /air/man?/ship?/
+                air = 'air'
+                man = 'man'
+                ship = 'ship'
+            ''',
+            expect_full_match=['air', 'airman', 'airship', 'airmanship'],
+            no_match=['manship'],
+            partial_match={'airma' : 'air'},
+        )
+
+        self.given('''
+            /ultra?/nagog/
+                ultra = "ultra"
+                nagog = 'nagog'
+            ''',
+            expect_full_match=['ultranagog', 'nagog'],
+            no_match=['ultrnagog'],
+        )
+
+        self.given('''
+            /cat?/fish?/
+                cat  = 'cat'
+                fish = 'fish'
+            ''',
+            expect_full_match=['catfish', 'cat', 'fish', ''],
+            partial_match={
+                'catfishing' : 'catfish', 
+                'cafis' : '',
+            }
+        )
+
+        self.given('''
+            /very?/very?/nice/
+                very = 'very '
+                nice = "nice"
+            ''',
+            expect_full_match=['nice', 'very nice', 'very very nice'],
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
